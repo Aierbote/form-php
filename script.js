fcdc244de87a5d2453bb9d0fc24afb8ef43c0b29
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-20 17:29:45 +0100

    fix: move 'const email' back in scope of inputs EventListener

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
   const button = document.querySelector('#submitButton');
+
   const select = document.querySelector('#form-service-choice');
 
   const inputs = form.querySelectorAll('input, select');
 
-  const email = (document.querySelector('input[type="email"]').value).trim();
   const regexEmail = /^[a-zA-Z0-9.-|(\+)]+@(\w+)(\.(\w{2,})){1,2}$/;
 
   inputs.forEach(input => {
     input.addEventListener('input', function () {
       let allAreValid = true;
+      const email = (document.querySelector('input[type="email"]').value).trim();
 
       if (select.value === 'default') {
         allAreValid = false;
@@ -31,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     });
+
   });
 
 });
